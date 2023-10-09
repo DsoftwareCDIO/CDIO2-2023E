46f7c5337857c0c629df05e07e8928252e78ea72
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-10-09 20:40:04 +0200

    Created AdventureGame

---
+++ src/game/main/AdventureGame.java
@@ -0,0 +1,9 @@
+package game.main;
+public class AdventureGame {
+    //private Player p1;
+    //private Player p2;
+    public static void main(String[] args) throws Exception {
+        //p1 = new Player("Player 1");
+        //p2 = new Player("Player 2");
+    }
+}
