aaea9a2f64815e1c26101779694ee5dc7d759eb0
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2023-10-09 21:00:02 +0200

    Commented out non existing features

--- src/game/main/AdventureGame.java
+++ src/game/main/AdventureGame.java
@@ -1,9 +1,26 @@
 package game.main;
+import java.util.Scanner;
 public class AdventureGame {
     //private Player p1;
     //private Player p2;
     public static void main(String[] args) throws Exception {
         //p1 = new Player("Player 1");
         //p2 = new Player("Player 2");
+        Scanner scanner = new Scanner(System.in);
+        
+        //Player currentPlayer = p1;
+        int turn = 1;
+        do {
+            // Set currentplayer depending on turn
+            //currentPlayer = turn % 2 == 0 ? p2 : p1;
+            System.out.println("\n" + "Round: " + (turn + 1) / 2);
+            //System.out.print(currentPlayer.getName() + "'s turn, press ENTER to roll the dice");
+            scanner.nextLine();
+
+            //d1.roll();
+            //d2.roll();
+            
+            turn++;
+        } while (/*!winCheck(currentPlayer, d1, d2)*/true);
     }
 }
