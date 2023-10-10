951840d9697c91aac7c49d505bd21b0788883193
TheSpicyOne <144691728+TheSpicyOne@users.noreply.github.com>
2023-10-10 15:44:46 +0200

    Comments added to account class

--- src/game/main/Account.java
+++ src/game/main/Account.java
@@ -1,15 +1,15 @@
 package game.main;
-
+//I'm rich, therfore im happy
 public class Account {
     private int coins = 0;
 
-    public Account(){};
+    public Account(){};     //Creates the account object
 
-    public int getCoins(){
+    public int getCoins(){      //Method to get coins
         return coins;
     }
 
-    public void changeCoins(int coins){
+    public void changeCoins(int coins){     //Method to change coins in Account
         this.coins += coins;
     }
 }
