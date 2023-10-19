9007654a0b4c8e7d3016bff2f037e614f04bc686
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-10-19 22:50:16 +0200

    DrawDice implemented

--- src/main/DiceCup.java
+++ src/main/DiceCup.java
@@ -3,6 +3,14 @@ import java.util.Random;
 
 
 public abstract class DiceCup {
+    private static String[] DiceVisuals = new String[] {
+        "+-------+\n|       |\n|   o   |\n|       |\n+-------+",
+        "+-------+\n| o     |\n|       |\n|     o |\n+-------+",
+        "+-------+\n| o     |\n|   o   |\n|     o |\n+-------+",
+        "+-------+\n| o   o |\n|       |\n| o   o |\n+-------+",
+        "+-------+\n| o   o |\n|   o   |\n| o   o |\n+-------+",
+        "+-------+\n| o   o |\n| o   o |\n| o   o |\n+-------+",
+    };
 
     public static int rollDice(int diceSides){
         // Init random
@@ -12,6 +20,16 @@ public abstract class DiceCup {
         int d1 = random.nextInt(diceSides) + 1;
         int d2 = random.nextInt(diceSides) + 1;
 
+        drawDice(new int[] {d1,d2});
         return(d1+d2);
+    }
+
+    private static void drawDice(int[] dice){
+        for (int i = 0; i < DiceVisuals[0].split("\n").length; i++) {
+            for (int die : dice) {
+                System.out.print(DiceVisuals[die-1].split("\n")[i]);
+            }
+            System.out.print("\n");
+        }
     }
 }
