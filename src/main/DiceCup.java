b6e6585cd4fa2bb1e16f199eb97580d7b580b011
Citronfly <37669985+Citronfly@users.noreply.github.com>
2023-10-24 13:23:11 +0200

    added commends on DiceCup

--- src/main/DiceCup.java
+++ src/main/DiceCup.java
@@ -6,6 +6,7 @@ public abstract class DiceCup {
     static Random random = new Random();
     
     private static String[] diceVisuals = new String[] {
+        //templates for the dice
         "+-------+\n|       |\n|   o   |\n|       |\n+-------+",
         "+-------+\n| o     |\n|       |\n|     o |\n+-------+",
         "+-------+\n| o     |\n|   o   |\n|     o |\n+-------+",
@@ -20,11 +21,13 @@ public abstract class DiceCup {
         int d1 = random.nextInt(diceSides) + 1;
         int d2 = random.nextInt(diceSides) + 1;
 
+        //draws the thown dice
         drawDice(new int[] {d1,d2});
         return(d1+d2);
     }
 
     private static void drawDice(int[] dice){
+        //writes up a visualization of the dice using a for loop
         for (int i = 0; i < diceVisuals[0].split("\n").length; i++) {
             for (int die : dice) {
                 System.out.print(diceVisuals[die-1].split("\n")[i] + "    ");
